e96772bdbdca0e58f8f60eb3b48cd59d1770d41d
8blank8 <roc.32@yandex.ru>
2023-05-27 17:46:53 +0700

    added delete all users

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -1,12 +1,14 @@
 import { Router, Request, Response } from "express";
 import { postsRepository } from "../repositories/posts-repository";
 import { blogsRepository } from "../repositories/blogs-repository";
+import { usersRepository } from "../repositories/users-repository";
 
 export const testingRouter = Router({})
 
 testingRouter.delete('/', (req: Request, res: Response) => {
     postsRepository.deleteAllPosts()
     blogsRepository.deleteAllBlogs()
+    usersRepository.deleteAllUsers()
      
     res.sendStatus(204)
      
