795a604740e6d4b136daabf4e83cd770d3caa8e3
8blank8 <roc.32@yandex.ru>
2023-07-02 18:56:21 +0700

    fix delete all devices

--- src/routes/security-router.ts
+++ src/routes/security-router.ts
@@ -27,14 +27,16 @@ async (req: Request, res: Response) => {
 })
 
 securityRouter.delete('/devices', 
-authMiddleware,
+refreshTokenMiddleware,
 async (req: Request, res: Response) => {
     const refreshToken = req.cookies.refreshToken
-    const userId = req.user!.id
+
+    const user = await jwtService.getFullUserByToken(refreshToken)
+    if(!user) return res.sendStatus(STATUS_CODE.UNAUTHORIZED_401)
 
     const deviceId = await jwtService.getDeviceIdByToken(refreshToken)
 
-    await securityService.deleteDevices(userId, deviceId)
+    await securityService.deleteDevices(user.id, deviceId)
 
     return res.sendStatus(STATUS_CODE.NO_CONTENT_204)
 })
